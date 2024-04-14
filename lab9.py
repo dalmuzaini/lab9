90519315aa2d1b5bede951b1d924f1e565feb8ca
dalmuzaini <166635334+dalmuzaini@users.noreply.github.com>
2024-04-13 22:38:15 -0400

    Decode function + option 2

--- lab9.py
+++ lab9.py
@@ -4,6 +4,10 @@ def encode_password(password):
     encoded = "".join(str((int(digit) + 3) % 10) for digit in password)
     return encoded
 
+def decode_password(encoded_password):
+    decoded = "".join(str((int(digit) - 3) % 10) for digit in encoded_password)
+    return decoded
+
 def menu():
     while True:
         print("\nMenu\n-------------")
@@ -16,6 +20,12 @@ def menu():
             print("Your password has been encoded and stored!")
             password_storage['encoded'] = encoded_password
             password_storage['password'] = password
+        elif option == "2":
+            if 'encoded' in password_storage:
+                encoded_password = password_storage['encoded']
+                password = password_storage['password']
+                decoded_password = decode_password(encoded_password)
+                print("The encoded password is {}, and the original password is {}.".format(encoded_password, password))
         elif option == "3":
             break
 
